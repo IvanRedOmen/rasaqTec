30a9f91269e8d584e304af0f3cc297fd3a26218d
Ivan Aragon Cruz <dark_hidden_memory@hotmail.com>
2018-01-07 21:16:34 -0600

    Initial commit

---
+++ ServicesApp/ServicesApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ServicesApp
+//
+//  Created by Ivan Aragon Cruz on 07/01/18.
+//  Copyright © 2018 Ivan Aragon Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
